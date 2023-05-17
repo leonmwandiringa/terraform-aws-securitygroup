fbe76c8855a5307960d5dc955842e1f49243dbb8
leonvodacom <leon.mwandiringa@vcontractor.co.za>
2023-05-17 20:54:18 +0200

    sg update

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,15 @@
 output "security_group_id" {
   value       =  aws_security_group.default.id
-  description = "EC2 instance Security Group ID"
+  description = "ENI Security Group ID"
 }
+
 
 output "security_group_arn" {
   value       = aws_security_group.default.arn
-  description = "EC2 instance Security Group ARN"
+  description = "ENI Security Group ARN"
 }
 
 output "security_group_name" {
   value       = aws_security_group.default.name
-  description = "EC2 instance Security Group name"
+  description = "ENI Security Group name"
 }
--- variables.tf
+++ variables.tf
@@ -1,35 +1,51 @@
 
-variable "global_tags" {
+variable "tags" {
   type = object({
+    Name = string
     Author      = string
     Environment = string
     Provisioner = string
     Region      = string
-    Name = string
   })
-  description = "base tags required in every resource"
+  description = "security group tags"
 }
 
-variable "sg_name" {
+variable "security_group_name" {
   type = string
   description = "security group name"
 }
 
-variable "sg_rules_egress" {
+variable "security_group_description" {
+  type = string
+  description = "security group description"
+}
+
+variable "security_group_rules_egress" {
   type = list(object({
     from_port   = number
     to_port     = number
     protocol    = string
-    cidr_blocks = string
+    cidr_blocks = list(string)
   }))
   description = "list of egress rules for the security group"
 }
 
 variable "vpc_id" {
   type = string
-  description = "vpc id for sg"
+  description = "vpc id for security_group"
 }
-variable "sg_rules_ingress" {
-  type = any
+
+variable "security_group_rules_ingress" {
+  type = list(object(
+    {
+      description = string
+      from_port   = number
+      to_port     = number
+      protocol    = string
+      cidr_blocks = list(string)
+      self = bool
+      security_groups = list(string)
+    }
+  ))
   description = "list of ingress rules for the security group"
 }
